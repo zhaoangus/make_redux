2da3d584f0bdd5a5fd86bc558cbc856f375acb9b
zhaoangus <zhao_angus@163.com>
2019-02-13 19:20:16 +0800

    project init

---
+++ src/index.js
@@ -0,0 +1,29 @@
+const appStore = {
+  title: {
+    text: 'React.js 小书',
+    color: 'red'
+  },
+  content: {
+    text: 'React.js 小书内容',
+    color: 'blue'
+  }
+}
+
+function renderApp (appState) {
+  renderTitle(appState.title)
+  renderContent(appState.content)
+}
+
+function renderTitle (title) {
+  const titleDOM = document.getElementById('title')
+  titleDOM.innerHTML = title.text
+  titleDOM.style.color = title.color
+}
+
+function renderContent (content) {
+  const contentDOM = document.getElementById('content')
+  contentDOM.innerHTML = content.text
+  contentDOM.style.color = content.color
+}
+
+renderApp(appStore)
