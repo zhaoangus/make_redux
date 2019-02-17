0188f44fbb4135ca55360a21980f4645c858a535
zhaoangus <zhao_angus@163.com>
2019-02-17 18:57:02 +0800

    v1

--- src/index.js
+++ src/index.js
@@ -1,45 +1,79 @@
-const appStore = {
-  title: {
-    text: 'React.js 小书',
-    color: 'red'
-  },
-  content: {
-    text: 'React.js 小书内容',
-    color: 'blue'
+function createStore (reducer) {
+  let state = null
+  const listeners = []
+  const subscribe = (listener) => listeners.push(listener)
+  const getState = () => state
+  const dispatch = (action) => {
+    state = reducer(state, action)
+    listeners.forEach(listener => listener())
   }
+  dispatch({})
+  return { getState, dispatch, subscribe }
 }
 
-function dispatch (action) {
+function renderApp (newAppState, oldAppState = {}) {
+  if (newAppState === oldAppState) return
+  renderTitle(newAppState.title)
+  renderContent(newAppState.content)
+}
+
+function renderTitle (newTitle, oldTitle = {}) {
+  if (newTitle === oldTitle) return
+  const titleDOM = document.getElementById('title')
+  titleDOM.innerHTML = newTitle.text
+  titleDOM.style.color = newTitle.color
+}
+
+function renderContent (newContent, oldContent = {}) {
+  if (newContent === oldContent) return
+  const contentDOM = document.getElementById('content')
+  contentDOM.innerHTML = newContent.text
+  contentDOM.style.color = newContent.color
+}
+
+function stateChanger (state, action) {
+  if (!state) {
+    return {
+      title: {
+        text: 'React.js 小书',
+        color: 'red'
+      },
+      content: {
+        text: 'React.js 小书内容',
+        color: 'blue'
+      }
+    }
+  }
   switch (action.type) {
     case 'UPDATE_TITLE_TEXT':
-      appStore.title.text = action.text
-      break
+      return {
+        ...state,
+        title: {
+          ...state.title,
+          text: action.text
+        }
+      }
     case 'UPDATE_TITLE_COLOR':
-      appStore.title.color = action.color
-      break
+      return {
+        ...state,
+        title: {
+          ...state.title,
+          color: action.color
+        }
+      }
     default:
-      break
+      return state
   }
 }
 
-function renderApp (appState) {
-  renderTitle(appState.title)
-  renderContent(appState.content)
-}
+const store = createStore(stateChanger)
+let oldState = store.getState()
+store.subscribe(() => {
+  const newState = store.getState()
+  renderApp(newState, oldState)
+  oldState = newState
+})
 
-function renderTitle (title) {
-  const titleDOM = document.getElementById('title')
-  titleDOM.innerHTML = title.text
-  titleDOM.style.color = title.color
-}
-
-function renderContent (content) {
-  const contentDOM = document.getElementById('content')
-  contentDOM.innerHTML = content.text
-  contentDOM.style.color = content.color
-}
-
-renderApp(appStore) // 首次渲染页面
-dispatch({ type: 'UPDATE_TITLE_TEXT', text: '《React.js 小书》' }) // 修改标题文本
-dispatch({ type: 'UPDATE_TITLE_COLOR', color: 'blue' }) // 修改标题颜色
-renderApp(appStore) // 把新的数据渲染到页面上
+renderApp(store.getState()) // 首次渲染页面
+store.dispatch({ type: 'UPDATE_TITLE_TEXT', text: '《React.js 小书》' }) // 修改标题文本
+store.dispatch({ type: 'UPDATE_TITLE_COLOR', color: 'green' }) // 修改标题颜色
